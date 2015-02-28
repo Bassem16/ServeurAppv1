0e053c981ff8b27df44e197c5d986499952a1128
webserver <webserver@ubuntu>
2015-02-28 13:20:59 +0100

    TP4, j'ai mis un squellette de HTML, c'est un free template, on pourra en utiliser un autre plus tard, mais c'est juste pour vous montrer ce que on peut faire (HTML PRESENTATION -> acceuil banque, il faudra au prealable avoir lancer le serveur) (SLIDE 12)

---
+++ BanqueTestWeb/src/fr/dauphine/mido/as/banquetest/beans/Operation.java
@@ -0,0 +1,16 @@
+package fr.dauphine.mido.as.banquetest.beans;
+
+public class Operation{
+private int idOperation;
+private String noCompte;
+private String dateOperation;
+private String heureOperation;
+private char operation;
+private float valeur;
+
+public Operation() {
+// TODO Auto-generatedconstructorstub
+}
+
+
+}
